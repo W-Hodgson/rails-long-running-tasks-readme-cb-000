c313f5385e00c63532cf92bb9e281aefeea92b64
Will Hodgson <will.hodgson@current-rms.com>
2019-11-26 15:19:12 +0000

    Automatically backed up by Learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,7 +6,7 @@ class CustomersController < ApplicationController
 
   def upload
     CSV.foreach(params[:leads].path, headers: true) do |lead|
-      Customer.create(email: lead[0], )
+      Customer.create(email: lead[0], first_name: lead[1])
     end
   end
 end
